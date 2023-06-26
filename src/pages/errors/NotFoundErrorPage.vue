7a9fcc429107bc2fd6de6489a17ccc40c8dc06ad
a2453540 <a2453540@gmail.com>
2023-06-26 15:30:36 +0800

    建立404

---
+++ src/pages/errors/NotFoundErrorPage.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="container py-5">
+        <h1>404</h1>
+        <p>對不起, <code>{{ $route.params.notFound }}</code>此頁面不存在</p>
+        <router-link :to="{name:'tasks'}">回首頁</router-link>
+    </div>
+</template>
