5b81a36e78bd69a1a493454bba3a9917fb54e965
a2453540 <a2453540@gmail.com>
2023-06-26 15:22:46 +0800

    設置當前活動鏈接的樣式

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,11 +12,11 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <!-- <router-link :to="{name:'tasks'}" class="nav-link">Tasks</router-link> -->
-                        <a href="#" @click.prevent="$router.push('/tasks')" class="nav-link">Task</a>
+                        <router-link :to="{name:'tasks'}" class="nav-link">Tasks</router-link>
+                        <!-- <a href="#" @click.prevent="$router.push('/tasks')" class="nav-link">Task</a> -->
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">Summary</a>
+                        <router-link :to="{name:'summary'}" class="nav-link">Summary</router-link>
                     </li>
                 </ul>
                 <ul class="navbar-nav ms-auto">
@@ -34,3 +34,10 @@
         </div>
     </nav>
 </template>
+
+<style scoped>
+.nav-link.router-link-active {
+    color: rgba(0,0,0,1);
+    font-weight:bolder ;
+}
+</style>
